b435c27bfe93ad79f0cdcb4b813b49a4af9f405b
srijankrs <srijankrsamanta@gmail.com>
2024-05-12 21:53:02 +0530

    added new products

--- src/Items.js
+++ src/Items.js
@@ -8,5 +8,35 @@ export const Items = [
         label:"Pouch Clip Sealer",
         link:"https://amzn.to/3y8IZO",
         imageLink:'./images/pouch-sealer-clip.jpg'
+    },
+    {
+        label:"Oil Brush Dispenser",
+        link:"https://www.wishlink.com/share/687gy",
+        imageLink:'./images/oil-brush.webp'
+    },
+    {
+        label:"Coffee frother",
+        link:"https://www.wishlink.com/share/gszaa",
+        imageLink:'./images/coffee-frother.webp'
+    },
+    {
+        label:"Soap Dispenser Brush",
+        link:"https://www.wishlink.com/share/2zqdw",
+        imageLink:'./images/soap-dispenser-brush.webp'
+    },
+    {
+        label:"Self Stirring Coffee Mug",
+        link:"https://www.wishlink.com/share/8jt47",
+        imageLink:'./images/self-sterring.webp'
+    },
+    {
+        label:"Waffle Maker",
+        link:"https://www.wishlink.com/share/mn4cb",
+        imageLink:'./images/waffle-maker.webp'
+    },
+    {
+        label:" Collapsible Funnel",
+        link:"https://www.wishlink.com/share/7jnfe",
+        imageLink:'./images/collapsable-funnel.webp'
     }
 ]
